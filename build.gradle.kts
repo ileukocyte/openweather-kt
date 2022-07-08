2d77ce6ca7cbba07fbcc3141b7df62a596f81a07
ileukocyte <alex.ilakeful@gmail.com>
2022-07-08 13:41:46 +0200

    maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,7 @@ plugins {
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
 
-    /*id("maven")
-    id("maven-publish")*/
+    id("maven-publish")
 
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("com.github.ben-manes.versions") version "0.42.0"
